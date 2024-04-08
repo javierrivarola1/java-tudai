e7ac9500b0524d065b9c555a74e1dc993d1e1e40
javierrivarola1 <156439571+javierrivarola1@users.noreply.github.com>
2024-04-08 11:13:04 -0300

    Add files via upload

---
+++ tudai/src/tudai/proj/Actividad5.java
@@ -0,0 +1,18 @@
+package tudai.proj;
+
+public class Actividad5 {
+
+	public static void main(String[] args) {
+	
+		String name = "Javier";
+		int edad = 30;
+		double altura = 1.76;
+		String ocupacion = "Sin trabajo";
+		
+		System.out.println(name);
+		System.out.println(edad);
+		System.out.println(altura);
+		System.out.println(ocupacion);
+	}
+
+}
---
+++ tudai/src/tudai/proj/Actividad8.java
@@ -0,0 +1,14 @@
+package tudai.proj;
+
+public class Actividad8 {
+
+	public static void main(String[] args) {
+
+		boolean verdadero = true;
+		boolean falso = false;
+		
+		System.out.println("El resultado para and es : " + (verdadero && falso));
+		System.out.println("El resultado para or es: " + (verdadero || false));
+	}
+
+}
