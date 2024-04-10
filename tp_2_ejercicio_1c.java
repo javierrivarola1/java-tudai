e38fef8ac782c1b2cb23f2b35af01fe89f5e8860
Javier Ignacio Rivarola <156439571+javierrivarola1@users.noreply.github.com>
2024-04-10 19:20:12 -0300

    Add files via upload

---
+++ tp_2_ejercicio_1c.java
@@ -0,0 +1,41 @@
+package tudai.proj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class tp_2_ejercicio_1c {
+
+	public static void main(String[] args) {
+		
+		BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+
+		int num;
+		
+		try {
+			
+			System.out.println("Brindame un numero :");
+			
+			num = Integer.valueOf(entrada.readLine());
+			
+			switch (num) {
+			case 1: System.out.println("Lunes");break;
+			case 2: System.out.println("Martes");break;
+			case 3: System.out.println("Miercoles");break;
+			case 4: System.out.println("Jueves");break;
+			case 5: System.out.println("Viernes");break;
+			
+			default: System.out.println("Es fin de semana");
+			
+			}
+
+			
+		}
+		
+		catch (Exception e) {
+			
+			System.out.println(e);
+		}
+
+	}
+
+}
