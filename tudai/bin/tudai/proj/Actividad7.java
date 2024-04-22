997ce863486735d974ba4e3de4d8ee2148db71ca
Javier Ignacio Rivarola <156439571+javierrivarola1@users.noreply.github.com>
2024-04-22 19:51:33 -0300

    Update Actividad7.java

--- tudai/bin/tudai/proj/Actividad7.java
+++ tudai/bin/tudai/proj/Actividad7.java
@@ -1,16 +1,28 @@
-package tudai.proj;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-public class Actividad7 {
+public class ej7 {
+    public static void main(String[] args) {
+        int num1, num2, num3;
+        float resultado;
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) {
-		
-		int num1 = 10;
-		int num2 = 2;
-		int num3 = 4;
-		
-		int resultado = (num1/num2) - num3;
+        try {
+            System.out.println("ingrese un numero entero 1");
+            num1 = Integer.valueOf(entrada.readLine());
 
-		System.out.println(resultado);
-	}
+            System.out.println("ingrese un numero 2");
+            num2 = Integer.valueOf(entrada.readLine());
 
+            System.out.println("ingrese un numero 3");
+            num3 = Integer.valueOf(entrada.readLine());
+
+            resultado = (num1 / num2) - num3;
+
+            System.out.println(resultado);
+
+        } catch (Exception exc) {
+            System.out.println(exc);
+        }
+    }
 }
