887b62810ac7f7056812988eb599b2d7f7a38503
Javier Ignacio Rivarola <156439571+javierrivarola1@users.noreply.github.com>
2025-03-27 10:55:15 -0300

    Add files via upload
    Volvemos al ruedo

---
+++ proyecto1/src/proyecto1/Proyecto1.java
@@ -0,0 +1,25 @@
+
+package proyecto1;
+
+
+public class Proyecto1 {
+
+    public static void main(String[] args) {
+        
+        //Registro automotor
+       String marcadeAuto = "Chevrolet";
+       int precio = 21000000;
+       double version = 2.8;
+       char categoria = 'b';
+       boolean autoNew = true;
+       
+        System.out.println("Marca del auto" + " " + marcadeAuto + "/n" + "hola");
+        System.out.println("Precio del mismo" + " " + precio);
+        System.out.println("Version del mismo" + " " +version);
+        System.out.println("Su categoria es" + " " +categoria);
+        System.out.println("Es un auto nuevo?" + " " + autoNew);
+       
+   
+    }
+   
+}
