746961096d1e2756d68342fbce7eaf024aa90be1
Javier Ignacio Rivarola <156439571+javierrivarola1@users.noreply.github.com>
2025-03-27 11:41:37 -0300

    Add files via upload
    ejercicio B-

---
+++ ejercicioDos/src/ejerciciodos/EjercicioDos.java
@@ -0,0 +1,24 @@
+
+package ejerciciodos;
+
+public class EjercicioDos {
+
+   
+    public static void main(String[] args)  {
+    
+        final double numeroUno = 1.0;
+
+
+        double numeroDos;
+
+   
+        numeroDos = numeroUno * 2;
+
+        System.out.println("El valor de la constante es: " + numeroUno);
+        System.out.println("El valor de la variable es: " + numeroDos);
+
+
+
+    }
+    
+}
