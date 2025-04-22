5d66a604d8470c2df66e4bb289386b1b32fdad09
Javier Ignacio Rivarola <156439571+javierrivarola1@users.noreply.github.com>
2025-04-22 11:24:02 -0300

    Add files via upload

---
+++ java_ejercicioBucles1/src/java_ejerciciobucles1/Java_ejercicioBucles1.java
@@ -0,0 +1,16 @@
+
+package java_ejerciciobucles1;
+
+public class Java_ejercicioBucles1 {
+
+    public static void main(String[] args) {
+        int contador = 0;
+        
+        while (contador<12) {
+            System.out.println("Mi contador va por la vuelta " + contador);
+            contador ++;
+        }
+
+    }
+    
+}
