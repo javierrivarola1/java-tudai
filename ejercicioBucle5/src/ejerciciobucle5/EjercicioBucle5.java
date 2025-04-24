4f68e16d2b2d9c9f7faddc670026459545af697f
Javier Ignacio Rivarola <156439571+javierrivarola1@users.noreply.github.com>
2025-04-24 12:50:13 -0300

    Add files via upload

---
+++ ejercicioBucle5/src/ejerciciobucle5/EjercicioBucle5.java
@@ -0,0 +1,16 @@
+
+package ejerciciobucle5;
+
+
+public class EjercicioBucle5 {
+
+
+    public static void main(String[] args) {
+        
+        for (int i = 200; i <= 250; i+=2) {
+            System.out.println("Mostrame todos los resultado sumando de dos en dos: " + i);
+        }
+        
+    }
+    
+}
