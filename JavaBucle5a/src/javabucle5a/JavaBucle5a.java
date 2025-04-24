129dac93652115880a313498cabac4e84b82322a
Javier Ignacio Rivarola <156439571+javierrivarola1@users.noreply.github.com>
2025-04-24 12:56:53 -0300

    Add files via upload

---
+++ JavaBucle5a/src/javabucle5a/JavaBucle5a.java
@@ -0,0 +1,15 @@
+
+package javabucle5a;
+
+
+public class JavaBucle5a {
+
+    public static void main(String[] args) {
+       
+        for (int i = 10; i >= 1; i--) {
+            System.out.println("Mostrarme la cuenta regresiva " + i);
+        }
+        
+    }
+    
+}
