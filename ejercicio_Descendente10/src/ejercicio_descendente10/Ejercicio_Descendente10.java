a81777048906dc3f653174723ab11d41599d234e
Javier Ignacio Rivarola <156439571+javierrivarola1@users.noreply.github.com>
2025-05-09 18:36:29 -0300

    Add files via upload

---
+++ ejercicio_Descendente10/src/ejercicio_descendente10/Ejercicio_Descendente10.java
@@ -0,0 +1,36 @@
+
+/*Escribir un programa que mientras el usuario ingrese un
+número entero entre 1 y 10 realice:
+
+a. Si el numero ingresado es múltiplo de 3 pida ingresar un
+caracter y para el caracter ingresado imprima a qué tipo de
+carácter está asociado:
+
+i. “letra minúscula” si el carácter es una letra del
+abecedario en minúscula;
+
+ii. “letra mayúscula” si el carácter es una letra del
+abecedario en mayúscula;
+
+iii. “dígito” si el carácter corresponde a un número;
+
+iv. “otro” para los restantes casos de caracteres.
+
+b. Si el número ingresado es múltiplo de 5 imprima la tabla de
+multiplicar del número ingresado.*/
+
+
+package ejercicio_descendente10;
+
+
+public class Ejercicio_Descendente10 {
+
+
+    public static void main(String[] args) {
+      
+        
+        
+        
+    }
+    
+}
