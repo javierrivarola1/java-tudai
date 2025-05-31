d3849a61bd3e1ff9ecc66d774fe9e179cc9e2fc8
Javier Ignacio Rivarola <156439571+javierrivarola1@users.noreply.github.com>
2025-05-31 15:43:46 -0300

    Add files via upload

---
+++ ejercicioArrays21/src/ejercicioarrays21/EjercicioArrays21.java
@@ -0,0 +1,43 @@
+
+package ejercicioarrays21;
+
+
+public class EjercicioArrays21 {
+
+    public static final int MAX = 12;
+
+    public static void main(String[] args) {
+        int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        int cantidadElementos = MAX; // Contador de elementos válidos
+
+        
+        cantidadElementos = filtrarImpares(array, cantidadElementos);
+        
+        imprimirResultado(array, cantidadElementos);
+    }
+
+    public static int filtrarImpares(int[] array, int cantidadElementos) {
+        for (int i = 0; i < cantidadElementos; i++) {
+            if (array[i] % 2 == 0) { // Si es par, lo eliminamos
+                cantidadElementos = corrimientoIzquierda(array, i, cantidadElementos);
+              
+            }
+        }
+        return cantidadElementos;
+    }
+
+    public static int corrimientoIzquierda(int[] array, int i, int cantidadElementos) {
+        for (int j = i; j < cantidadElementos - 1; j++) {
+            array[j] = array[j + 1]; // Movemos elementos hacia la izquierda
+        }
+        cantidadElementos--; // Reducimos la cantidad de elementos válidos
+        return cantidadElementos;
+    }
+
+    public static void imprimirResultado(int[] array, int cantidadElementos) {
+        for (int i = 0; i < cantidadElementos; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+}
