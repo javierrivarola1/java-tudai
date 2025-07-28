ecd742651d380c68ba3d2bd349f56d76e880d93c
Javier Ignacio Rivarola <156439571+javierrivarola1@users.noreply.github.com>
2025-07-28 09:51:24 -0300

    Add files via upload

---
+++ ejercicio_revision_selectivas2/src/ejercicio_revision_selectivas2/Ejercicio_revision_selectivas2.java
@@ -0,0 +1,27 @@
+/*
+Escribir un programa que declare una variable de tipo double y una
+constante de tipo double con valor 1.0. Luego, deberá asignar el
+doble del valor de la constante a la variable y mostrar ambos por
+pantalla.
+
+ */
+package ejercicio_revision_selectivas2;
+
+
+public class Ejercicio_revision_selectivas2 {
+
+
+    public static void main(String[] args) {
+        
+        double num1 = 0.1;
+        final double num2 = 1.0;
+        double num3;
+        
+        num3 = (1.0*2) + num1;
+        
+        System.out.println("Resultado del num3 " + num3 + " y num2 es: " + num2);
+        
+       
+    }
+    
+}
