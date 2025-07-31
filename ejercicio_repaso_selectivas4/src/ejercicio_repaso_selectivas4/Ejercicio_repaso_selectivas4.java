a05ffa8c24c96573d7d8d6042ffc4d2a77fa0843
Javier Ignacio Rivarola <156439571+javierrivarola1@users.noreply.github.com>
2025-07-31 16:53:54 -0300

    Add files via upload

---
+++ ejercicio_repaso_selectivas4/src/ejercicio_repaso_selectivas4/Ejercicio_repaso_selectivas4.java
@@ -0,0 +1,23 @@
+/*
+Escribir un programa que permita ingresar 5 números de a uno por
+vez y que los muestre por pantalla en orden inverso:
+
+ */
+package ejercicio_repaso_selectivas4;
+
+
+public class Ejercicio_repaso_selectivas4 {
+
+   
+    public static void main(String[] args) {
+        int num1 = 0;
+        int num2 = 1;
+        int num3 = 2;
+        int num4 = 3;
+        int num5 = 4;
+        
+        System.out.println("Los numeros pedidos son: " + num1 + num2 + num3 + num4 + num5);
+        System.out.println("Inverso : " + num5 + num4 + num3 + num2 + num1);
+    }
+    
+}
