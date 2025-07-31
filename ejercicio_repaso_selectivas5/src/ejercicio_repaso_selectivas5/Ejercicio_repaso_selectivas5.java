121b985e965b2cb5722fa6b61f7dba8066a27956
Javier Ignacio Rivarola <156439571+javierrivarola1@users.noreply.github.com>
2025-07-31 17:27:52 -0300

    Add files via upload

---
+++ ejercicio_repaso_selectivas5/src/ejercicio_repaso_selectivas5/Ejercicio_repaso_selectivas5.java
@@ -0,0 +1,23 @@
+/*
+ Escribir un programa que solicite nombre, edad, altura y ocupación, y
+los imprima por pantalla.
+
+ */
+package ejercicio_repaso_selectivas5;
+
+
+public class Ejercicio_repaso_selectivas5 {
+
+   
+    public static void main(String[] args) {
+      
+        String nombre = "Javier";
+        int edad = 32;
+        double altura = 1.77;
+        String ocupacion = "Soltero";
+        
+        System.out.println("Los datos son: " + "nombre : " + " " + nombre  + " " + "edad : " + " " +  edad + " " + "altura : " + " " + altura + " " + "ocupacion : " + " " + ocupacion);
+        
+    }
+    
+}
